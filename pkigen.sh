6a12dafcda2548a23a82d20c2e533e2714bd71eb
Bogdan Shchogolev <bogdan993000@gmail.com>
2022-01-25 15:27:19 +0200

    Add script for PKI initialization
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

---
+++ pkigen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+ersa=/usr/share/easy-rsa/easyrsa
+
+$ersa init-pki
+echo -e "${CA_PASS}\n${CA_PASS}\n" | $ersa build-ca
+
+echo | $ersa gen-req dockervpn nopass
+echo -e "yes\n${CA_PASS}" | $ersa sign-req server dockervpn
+
+openssl dhparam -dsaparam -out dh.pem 4096
+
+openvpn --genkey tls-auth ta.key
