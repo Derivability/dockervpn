dae7033e449832c942368daa35a298c3faa56042
Bogdan Shchogolev <bogdan993000@gmail.com>
2022-01-29 18:41:21 +0200

    Switch to absolute paths in pkigen.sh
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- src/vpn/scripts/pkigen.sh
+++ src/vpn/scripts/pkigen.sh
@@ -2,10 +2,11 @@
 set -e
 
 ersa=/usr/share/easy-rsa/easyrsa
+PKI_DIR=/vpn/pki
 cd /vpn
 
 CA_PASS=$(cat pki/ca_pass)
-rm -rf pki/ca_pass
+rm -rf ${PKI_DIR}/ca_pass
 
 $ersa init-pki
 echo -e "${CA_PASS}\n${CA_PASS}\n" | $ersa build-ca
@@ -13,6 +14,6 @@ echo -e "${CA_PASS}\n${CA_PASS}\n" | $ersa build-ca
 echo | $ersa gen-req dockervpn nopass
 echo -e "yes\n${CA_PASS}" | $ersa sign-req server dockervpn
 
-openssl dhparam -dsaparam -out dh.pem 4096
+openssl dhparam -dsaparam -out ${PKI_DIR}/dh.pem 4096
 
-openvpn --genkey tls-crypt pki/ta.key
+openvpn --genkey tls-crypt ${PKI_DIR}/ta.key
